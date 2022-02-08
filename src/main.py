8a647b36739a63120f5976a52b4287ae6260dff6
cmsaravanan99 <75291650+cmsaravanan99@users.noreply.github.com>
2022-02-08 08:28:46 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-
+from.import Datastage_Profiler
+from.import Datastage_Conversion
+def in_path():
+  in_path=r'c:\Users\cmsar\Downloads\input'
+  retirn in_path
+def out_path():
+  out_path=r'c:\Users\cmsar\Downloads\input'
+  return out_path
