f695651cdc07ad036efb1e8271c67ba2cfba0a6a
cmsaravanan99 <75291650+cmsaravanan99@users.noreply.github.com>
2022-02-08 08:33:42 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,13 @@
-
+from.import main
+import xmltodict
+import os
+in_path=main.in_path()
+out_path=main.out_path()
+def ord_dict():
+  xml_str=open(in_file).read()
+  ord_dict=xmltodict.parse(xml_str)
+  return ord_dict
+files=os.listdir(in_path)
+for file in files:
+  if '.xml' in files:
+    in_file=in_path+'//'+file
